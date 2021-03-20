ee55c217379ad146e20e193e9a26e10cb749d6c5
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-20 10:32:19 +0100

    Update logo and Add basic scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import { ReactComponent as Logo } from "./logo.svg";
 import "./App.scss";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
