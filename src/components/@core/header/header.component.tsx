3e8cd1cbc868325d74f001adaa6f02c8546f9bf2
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-20 11:35:05 +0100

    Clean logo AND add Header

--- src/components/@core/header/header.component.tsx
+++ src/components/@core/header/header.component.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
+import { UrlsConst } from "../../../constants/urls.constants";
 
 const Header = () => (
-  <header>
-    <Logo className="header__logo" />
+  <header className="header">
+    <Link className="logo-container" to={UrlsConst.ROOT}>
+      <figure className="header__figure">
+        <Logo className="header__logo" />
+        <figcaption className="header__figcaption">
+          Rick and Morty logo
+        </figcaption>
+      </figure>
+    </Link>
   </header>
 );
 
