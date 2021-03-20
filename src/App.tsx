678ac90f60c295744b02573533e2e2b0f877aad1
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-20 10:44:48 +0100

    Clean code and Add some colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,7 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <Logo />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo className="logo" />
       </header>
     </div>
   );
