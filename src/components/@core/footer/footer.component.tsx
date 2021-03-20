96a2d1fb36694964011d997678df838f19d37745
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-20 22:50:51 +0100

    Fix generic style

--- src/components/@core/footer/footer.component.tsx
+++ src/components/@core/footer/footer.component.tsx
@@ -6,23 +6,25 @@ import { ReactComponent as LinkedIn } from '../../../assets/images/linkedin.svg'
 
 const Footer = ({ t }: ITranslation): JSX.Element => (
   <footer className="footer">
-    <p className="footer__copyright">
-      {t('footer.copyright.label')} {new Date().getFullYear()}
-    </p>
-    <div className="footer__icons">
-      <figure className="footer__icon">
-        <a
-          aria-label={t('footer.linkedin.label')}
-          href="https://linkedin.com/in/baran-roman/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedIn />
-        </a>
-        <figcaption className="footer__icon-caption--hidden">
-          {t('footer.linkedin.icon')}
-        </figcaption>
-      </figure>
+    <div className={'footer__container'}>
+      <p className="footer__copyright">
+        {t('footer.copyright.label')} {new Date().getFullYear()}
+      </p>
+      <div className="footer__icons">
+        <figure className="footer__icon">
+          <a
+            aria-label={t('footer.linkedin.label')}
+            href="https://linkedin.com/in/baran-roman/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedIn />
+          </a>
+          <figcaption className="footer__icon-caption--hidden">
+            {t('footer.linkedin.icon')}
+          </figcaption>
+        </figure>
+      </div>
     </div>
   </footer>
 );
