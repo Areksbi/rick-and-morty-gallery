d34c2875b67457ded262bcf449f2adcba5990174
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-21 16:43:37 +0100

    Fix Modal style

--- src/components/@shared/modal/modal.component.tsx
+++ src/components/@shared/modal/modal.component.tsx
@@ -19,14 +19,16 @@ const Modal = ({ dispatchHideModal, modal }: ModalProps) => {
   const onCloseButtonClick = () => dispatchHideModal();
 
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <span className="modal-close" onClick={onCloseButtonClick}>
-          &#10005;
-        </span>
-        <h3>{modal.title}</h3>
-        <div>{modal.content}</div>
-      </div>
+    <div className="modal__overlay">
+      <article className="modal">
+        <h3 className={'modal__title'}>
+          <span>{modal.title}</span>
+          <button className="modal__close" onClick={onCloseButtonClick}>
+            &#10005;
+          </button>
+        </h3>
+        <section className={'modal__content'}>{modal.content}</section>
+      </article>
     </div>
   );
 };
