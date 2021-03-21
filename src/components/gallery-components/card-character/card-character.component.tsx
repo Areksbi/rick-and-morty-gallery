f7698c40ee140d782cdc5da26d249448fcdb8c34
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-22 00:53:54 +0100

    Clear card component

--- src/components/gallery-components/card-character/card-character.component.tsx
+++ src/components/gallery-components/card-character/card-character.component.tsx
@@ -3,6 +3,7 @@ import { connect, ConnectedProps } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
 import './card-character.styles.scss';
+import { ApiRickAndMorty } from '../../../constants/api.constants';
 import { ApiRickAndMortyLocationEnums, ApiRickAndMortyStatusEnum } from '../../../enums/api-rick-and-morty.enums';
 import {
   IApiRickAndMortyCharactersResult,
@@ -12,7 +13,6 @@ import {
 } from '../../../interfaces/api-rick-and-morty.interfaces';
 import { showModal } from '../../../store/modal/modal.actions';
 import { TranslationsEnums } from '../../../enums/translations.enums';
-import { ApiRickAndMorty } from '../../../constants/api.constants';
 import TableEpisodes from '../table-episodes/table-episodes.components';
 import TableLocation from '../table-location/table-location.component';
 
@@ -22,16 +22,15 @@ type CardProps = IApiRickAndMortyCharactersResult & ConnectedProps<typeof connec
 
 const Card = ({ name, image, gender, location, origin, species, status, type, episode, dispatchShowModal }: CardProps): JSX.Element => {
   const [t] = useTranslation(TranslationsEnums.COMMON);
+
   const onEpisodesClick = async () => {
     // @ts-ignore
-    const listEpisodes: string[] = episode
-      .map((ep: string) => ep.split('/').pop())
-      .filter((ep: string | undefined) => typeof ep === 'string');
+    const listEpisodes: string[] = episode.map((ep: string) => ep.split('/').pop());
     const res = await fetch(ApiRickAndMorty.EPISODES(listEpisodes));
-    const json: IApiRickAndMortyEpisode[] = await res.json();
+    const episodes: IApiRickAndMortyEpisode[] = await res.json();
     dispatchShowModal({
       title: `${t('gallery.character.episodesWith')} ${name}`,
-      content: <TableEpisodes episodes={json} />,
+      content: <TableEpisodes episodes={episodes} />,
     });
   };
   const onLocationClick = async (locationToSearch: IApiRickAndMortyLocation) => {
@@ -39,12 +38,19 @@ const Card = ({ name, image, gender, location, origin, species, status, type, ep
     if (!locationId) return;
 
     const res = await fetch(ApiRickAndMorty.LOCATION(locationId));
-    const json: IApiRickAndMortyLocationResponse = await res.json();
+    const locResp: IApiRickAndMortyLocationResponse = await res.json();
     dispatchShowModal({
       title: `${locationToSearch.name}`,
-      content: <TableLocation residents={json.residents.length} type={json.type} dimension={json.dimension} />,
+      content: <TableLocation residents={locResp.residents.length} type={locResp.type} dimension={locResp.dimension} />,
     });
   };
+  const tableRow = (field: string, value: JSX.Element | string) =>
+    value ? (
+      <tr>
+        <td>{field}</td>
+        <td>{value}</td>
+      </tr>
+    ) : null;
 
   return (
     <article className="card">
@@ -56,34 +62,23 @@ const Card = ({ name, image, gender, location, origin, species, status, type, ep
       </header>
       <table className="card__info">
         <tbody>
-          <tr>
-            <td>{t('gallery.character.species')}</td>
-            <td>{species}</td>
-          </tr>
-          {type ? (
-            <tr>
-              <td>{t('gallery.character.type')}</td>
-              <td>{type}</td>
-            </tr>
-          ) : null}
-          <tr>
-            <td>{t('gallery.character.gender')}</td>
-            <td>{gender}</td>
-          </tr>
-          <tr>
-            <td>{t('gallery.character.status')}</td>
-            <td className="card__status">
+          {tableRow(t('gallery.character.species'), species)}
+          {tableRow(t('gallery.character.type'), type)}
+          {tableRow(t('gallery.character.gender'), gender)}
+          {tableRow(
+            t('gallery.character.status'),
+            <span className="card__status">
               {status === ApiRickAndMortyStatusEnum.ALIVE ? (
                 <span className="card__status-icon card__status-icon--alive"></span>
               ) : (
                 status === ApiRickAndMortyStatusEnum.DEAD && <span className="card__status-icon card__status-icon--dead"></span>
               )}
               {status}
-            </td>
-          </tr>
-          <tr>
-            <td>{t('gallery.character.location')}</td>
-            <td>
+            </span>
+          )}
+          {tableRow(
+            t('gallery.character.location'),
+            <>
               {location.name === ApiRickAndMortyLocationEnums.UNKNOWN ? (
                 <span>{location.name}</span>
               ) : (
@@ -92,11 +87,11 @@ const Card = ({ name, image, gender, location, origin, species, status, type, ep
                   <span className={'card__location-label'}>{location.name} &#8599;</span>
                 </button>
               )}
-            </td>
-          </tr>
-          <tr>
-            <td>{t('gallery.character.origin')}</td>
-            <td>
+            </>
+          )}
+          {tableRow(
+            t('gallery.character.origin'),
+            <>
               {origin.name === ApiRickAndMortyLocationEnums.UNKNOWN ? (
                 <span>{origin.name}</span>
               ) : (
@@ -105,8 +100,8 @@ const Card = ({ name, image, gender, location, origin, species, status, type, ep
                   <span className={'card__origin-label'}>{origin.name} &#8599;</span>
                 </button>
               )}
-            </td>
-          </tr>
+            </>
+          )}
           <tr>
             <td className={'card__episodes'} colSpan={2}>
               <button className={'card__episodes-button'} onClick={onEpisodesClick}>
