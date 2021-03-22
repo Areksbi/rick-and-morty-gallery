d6b2fc64fdc92b3ba7b216567e7daf8eaf9a2483
Areksbi <36035819+Areksbi@users.noreply.github.com>
2021-03-22 01:21:28 +0100

    Fix a11y input

--- src/components/@shared/input/input.component.tsx
+++ src/components/@shared/input/input.component.tsx
@@ -3,7 +3,7 @@ import { IInputProps } from './input.interfaces';
 
 const Input = ({ handleChange, label, id, ...otherProps }: IInputProps) => (
   <div className="input">
-    <input className="input__field" {...otherProps} onChange={handleChange} />
+    <input className="input__field" id={id} {...otherProps} onChange={handleChange} />
     {label && (
       <label htmlFor={id} className={`${otherProps.value?.length ? 'input__label--shrink' : ''} input__label`}>
         {label}
